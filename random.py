f9caeb66111a48e3e0b345856c2002322cf87434
Eunkyoung Kim <irinakim1225@gmail.com>
2022-09-19 07:35:00 +0900

    Added SSD1306 OLED output

--- random.py
+++ random.py
@@ -1,6 +1,11 @@
 from machine import Pin,  SPI, I2C
-import ssd1306
 import random
+from ssd1306 import SSD1306_I2C
+from time import sleep
+
+
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
 result = []
 #for i in range(6):
 #    num = random.randrange(1,46)
@@ -11,9 +16,7 @@ result = []
 #        print("%d Again\r\n "%i)
 #    else:
 #        result.insert(0,num)
-i2c = I2C(sda=machine.Pin(23), scl=machine.Pin(22))
-i2c.scan()
-        
+#         
 while True:
     num = random.randrange(1,46)
     if num in result:
@@ -25,5 +28,15 @@ while True:
     if 6 == len(result):
         break
 result.sort() 
+
+oled.text("Lottery numbers.",1,0)
+oled.show()
+
+oled.text(str(result[:3]), 0, 10)
+oled.text(str(result[3:6]), 0, 20)
+oled.text("!!!Good Luck!!!", 0, 32)
+oled.show()
 print(result)
 del result
+
+
